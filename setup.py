1c7813e8dd6b29ab193ba86a273aad5a21935478
Seema Mishra <seema.mishra6@gmail.com>
2022-07-31 13:49:41 -0700

    Added Cookicutter structure and Coda environemnt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project uses Deep Leaning to detect lung infection in images of lungs.',
+    author='Seema Mishra',
+    license='MIT',
+)
